5fb7f053e42f4859559dd4bf927ee64263857c89
Nobu19800 <TyouKyozyakuTaisitu@yahoo.co.jp>
2014-12-01 20:06:10 +0900

    2006

---
+++ CalcWindowSize.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import ctypes
+ 
+def main():
+    wx = ctypes.windll.user32.GetSystemMetrics(0)
+    wy = ctypes.windll.user32.GetSystemMetrics(1)
+
+    print u"スライドの幅を入力してください"
+    sx = float(sys.stdin.readline())
+    print u"スライドの高さを入力してください"
+    sy = float(sys.stdin.readline())
+
+    w = int(sx/sy*wy)
+
+    ans = int((wx - w)/2)
+
+    
+    print "左上の座標",ans,0
+    print "幅、高さ",w,wy
+    
+ 
+if __name__ == "__main__":
+    main()
